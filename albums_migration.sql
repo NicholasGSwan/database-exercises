196dd2ae2610ca4b8f043f500db8d013c1184cf0
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-10 13:59:13 -0500

    Create albums_seeder.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100),
-  name VARCHAR(150) NOT NULL,
-  release_date DATE,
-  sales FLOAT,
-  genre VARCHAR(50),
+  artist VARCHAR(200) NOT NULL,
+  name VARCHAR(300) NOT NULL,
+  release_date DATE NOT NULL,
+  sales DECIMAL(10,2)NOT NULL,
+  genre VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 )
---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson','Thriller',1982,47.3,'Pop, Rock, R&B' );
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971-1975)',1976,32.2, 'Rock, Soft Rock, Folk Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC','Back in Black',1980,26.1,'Hard Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd','The Dark Side of the Moon', 1973, 24.2, 'Progressive Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop, Soundtrack');
+INSERT INTO albums (artist, name, release_date, sales, genre)
