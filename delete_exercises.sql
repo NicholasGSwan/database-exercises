f6a1823185b0ceb010c1f991f080d5e1fc08b7e1
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-11 15:12:03 -0500

    -convert select to delete -complete exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,3 +1,4 @@
-SELECT * FROM albums WHERE release_date >1991;
-SELECT * FROM albums WHERE genre LIKE '%Disco%';
-SELECT * FROM albums WHERE artist = 'Celine Dion';
+USE codeup_test_db;
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre LIKE '%Disco%';
+DELETE FROM albums WHERE artist = 'Celine Dion';
