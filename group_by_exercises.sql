785728c117249d70e56dbb8bf47b5fae344f340f
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-12 14:26:49 -0500

    Finish group by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,7 +4,13 @@ SELECT title
 FROM titles
 GROUP BY title ASC;
 
-SELECT first_name, last_name
+SELECT
+  first_name,
+  last_name
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
 GROUP BY last_name, first_name;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
