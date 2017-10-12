7d2a44960e73f72ffbb3bf3babcb82b85ab11249
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-12 09:45:06 -0500

    changed IN to OR for first names

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,9 @@
 USE employees;
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
-SELECT * From employees where birth_date LIKE '%-12-25';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+# SELECT * FROM employees WHERE last_name LIKE 'E%';
+# SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
+# SELECT * From employees where birth_date LIKE '%-12-25';
+# SELECT * FROM employees WHERE last_name LIKE '%q%';
