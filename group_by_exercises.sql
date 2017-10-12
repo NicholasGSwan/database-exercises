b4e3d2ea77dcc2c07f331d37356badd1a42ac3c0
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-12 14:25:37 -0500

    Complete steps 4 and 5 on Group by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,6 +1,10 @@
-use employees;
+USE employees;
 DESCRIBE titles;
 SELECT title
 FROM titles
-GROUP BY title asc;
+GROUP BY title ASC;
 
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+GROUP BY last_name, first_name;
