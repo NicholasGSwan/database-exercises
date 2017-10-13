c359c85e77f856abadc8609d455a1d5de222371e
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-13 09:27:11 -0500

    complete functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -11,3 +11,4 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date desc;
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 SELECT DATEDIFF(CURRENT_DATE(), hire_date) AS 'How Many Days at Company' FROM employees WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date desc;
+SELECT count(CONCAT(e.first_name,' ', e.last_name)) AS Number_Of, CONCAT(e.first_name,' ', e.last_name) AS Full_Name FROM employees e WHERE e.last_name LIKE '%q%' AND e.last_name NOT LIKE '%qu%' GROUP By Full_Name ORDER BY Number_Of desc;
