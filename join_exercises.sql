6e471518fdd6b2e4ae23621d66792dbb9e56c135
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-13 16:09:19 -0500

    finish join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -28,3 +28,11 @@ on d.dept_no = de.dept_no
 where d.dept_name = 'Customer Service' and t.to_date = '9999-01-01' and de.to_date = '9999-01-01'
 GROUP BY t.title
 
+SELECT d.dept_name as 'Department Name', concat(e.first_name, ' ',e.last_name), s.salary as Salary From employees as e JOIN dept_manager as dm
+    on dm.emp_no = e.emp_no
+  JOIN departments as d
+    on d.dept_no = dm.dept_no
+  JOIN salaries as s
+    on s.emp_no = dm.emp_no
+where s.to_date = '9999-01-01' and dm.to_date = '9999-01-01' order by d.dept_name;
+
