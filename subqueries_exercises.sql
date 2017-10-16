0cdbc438ea9aaab2d1c8a02e9d92f3df8a284458
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-16 09:16:35 -0500

    complete 3rd query

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,8 +6,14 @@ SELECT concat(first_name, ' ', last_name) as 'Full Name', emp_no, hire_date FROM
   where emp_no = 101010
 );
 
-SELECT t.title as Title, count(t.title) from titles as t where emp_no in (
+SELECT t.title as Title, count(t.title) as 'Number of Aamods With Title' from titles as t where emp_no in (
   SELECT emp_no
   From employees
   where first_name = 'Aamod'
 ) GROUP BY t.title;
+
+SELECT first_name, last_name FROM employees where emp_no in (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date > current_date()
+) and gender = 'F';
