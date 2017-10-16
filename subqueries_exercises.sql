d4a497cdc54c45fdf528a1863f11d1a4f278ebee
Uglygrayduck <Nicholasgswan@gmail.com>
2017-10-16 09:12:31 -0500

    complete second query

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -4,4 +4,10 @@ SELECT concat(first_name, ' ', last_name) as 'Full Name', emp_no, hire_date FROM
     SELECT hire_date
     FROM employees
   where emp_no = 101010
-)
+);
+
+SELECT t.title as Title, count(t.title) from titles as t where emp_no in (
+  SELECT emp_no
+  From employees
+  where first_name = 'Aamod'
+) GROUP BY t.title;
